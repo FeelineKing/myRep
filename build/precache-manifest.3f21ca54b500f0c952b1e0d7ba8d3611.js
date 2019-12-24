8515fa24d4fcbd92cdc83c09a799c161379b118b
Leon <FeelineKing@gmail.com>
2019-12-24 14:26:51 +0200

    secondCommit

---
+++ build/precache-manifest.3f21ca54b500f0c952b1e0d7ba8d3611.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96128b3958514f45121a1c4a26bfcd1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a76198e5c3401f8e4209",
+    "url": "/static/css/main.ba4c65fa.chunk.css"
+  },
+  {
+    "revision": "f079f4b86cff5b998ab0",
+    "url": "/static/js/2.c82170eb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.c82170eb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a76198e5c3401f8e4209",
+    "url": "/static/js/main.f887b013.chunk.js"
+  },
+  {
+    "revision": "42ac96cd1b05ea8627aa",
+    "url": "/static/js/runtime-main.e924cef8.js"
+  },
+  {
+    "revision": "a683f98e087fe7441e4e94accc5ce6d6",
+    "url": "/static/media/KotomineKirei.a683f98e.png"
+  },
+  {
+    "revision": "fbe65262ad9fdd3411b2f411d6241d26",
+    "url": "/static/media/apocalypse-mask.fbe65262.png"
+  },
+  {
+    "revision": "1af68967c1a35c05fd2def16e452b9b5",
+    "url": "/static/media/apocalypse01.1af68967.jpg"
+  },
+  {
+    "revision": "724b5f9a1c69904e257044274581e290",
+    "url": "/static/media/apocalypse02.724b5f9a.jpg"
+  },
+  {
+    "revision": "6daf9ddbf2cd5b009dd0aaf5d72755b1",
+    "url": "/static/media/apocalypse03.6daf9ddb.jpg"
+  },
+  {
+    "revision": "8b73e2ae62aa1d7b154dfa449239d7bd",
+    "url": "/static/media/apocalypse04.8b73e2ae.jpg"
+  },
+  {
+    "revision": "6fbe72eb89939866ea922e3f76c109e8",
+    "url": "/static/media/archerFemMask.6fbe72eb.png"
+  },
+  {
+    "revision": "85c1d32ab9380ae1be69b7ee2d5d5520",
+    "url": "/static/media/assassinFemMask.85c1d32a.png"
+  },
+  {
+    "revision": "478a100c27ab824a6018f3e72a00cc43",
+    "url": "/static/media/berserkFemMask.478a100c.png"
+  },
+  {
+    "revision": "06484453142c39c990f9c4e95897a270",
+    "url": "/static/media/casterFemMask.06484453.png"
+  },
+  {
+    "revision": "e18836561871f0012cae981e6f9151bb",
+    "url": "/static/media/femArcher.e1883656.png"
+  },
+  {
+    "revision": "5892a9b1a55cba6c0d13f7dfc82d5e46",
+    "url": "/static/media/femAssassin.5892a9b1.png"
+  },
+  {
+    "revision": "1370b3f041a535cc944f719d323355ce",
+    "url": "/static/media/femBerserk.1370b3f0.png"
+  },
+  {
+    "revision": "b3ba0e418e2e976fa56a0607bda6e140",
+    "url": "/static/media/femCaster.b3ba0e41.png"
+  },
+  {
+    "revision": "a50a9b78b04aa5178cc7afc6a4da69da",
+    "url": "/static/media/femLancer.a50a9b78.png"
+  },
+  {
+    "revision": "322807faf9005a1d62a518aff3445373",
+    "url": "/static/media/femRider.322807fa.png"
+  },
+  {
+    "revision": "8d237660a90efae756f602d13e6ddfb9",
+    "url": "/static/media/femSaber.8d237660.png"
+  },
+  {
+    "revision": "5a52cebba38bc2fb1e0ecc4531f5ee38",
+    "url": "/static/media/introAudio.5a52cebb.mp3"
+  },
+  {
+    "revision": "d94527867350bdb61e22b0d5df9bb27e",
+    "url": "/static/media/lancerFemMask.d9452786.png"
+  },
+  {
+    "revision": "f56012f1f705adc177ffad7d64363801",
+    "url": "/static/media/logo.f56012f1.png"
+  },
+  {
+    "revision": "8e485596059c280ca6ca687b72b0c22f",
+    "url": "/static/media/person-name.8e485596.png"
+  },
+  {
+    "revision": "c4e61824dde1410f1474f2fa2721d6eb",
+    "url": "/static/media/riderFemMask.c4e61824.png"
+  },
+  {
+    "revision": "9bb90ec18b9e2f67a159d8f5c9a8c21e",
+    "url": "/static/media/saberFemMask.9bb90ec1.png"
+  },
+  {
+    "revision": "c5c561e7ef5e0f69a67fd4008f994fa6",
+    "url": "/static/media/settingsBg.c5c561e7.png"
+  },
+  {
+    "revision": "41026f0ef6317a17206bd225ac4ef004",
+    "url": "/static/media/speakerFull.41026f0e.png"
+  },
+  {
+    "revision": "63d132800321b09c19cc1a2c0f2348da",
+    "url": "/static/media/speakerLow.63d13280.png"
+  },
+  {
+    "revision": "e3787c2ce8f1e83c25ba9d7f815e38f1",
+    "url": "/static/media/speakerMid.e3787c2c.png"
+  },
+  {
+    "revision": "d85465669cc0013e9f3ad98dabc32450",
+    "url": "/static/media/start-page.d8546566.jpg"
+  }
+]);
